22cf0a18e38858d6ed5ae2b6ff5afb79755b3415
shouryaverma <46604683+shouryaverma@users.noreply.github.com>
2022-11-19 13:31:05 +0100

    only 1.6 left

--- ex1/exercise_1.py
+++ ex1/exercise_1.py
@@ -4,6 +4,7 @@
 # Author: Your Name <your@email.com>
 import numpy as np
 import itertools
+from collections import namedtuple
 
 # For exercise 1.2
 def evaluate_energy(nodes, edges, assignment):
@@ -37,19 +38,100 @@ def bruteforce(nodes, edges):
 
 
 # For exercise 1.4
+def get_Fn(processed_nodes):
+    F =[]
+    for node in processed_nodes:
+        labels =[]
+        for label in node.processed_labels:
+            labels.append (label.F)
+        F.append(labels)   
+    return F
+
+def get_Theta(processed_nodes):
+    Theta =[]
+    for node in processed_nodes:
+        labels =[]
+        for label in node.processed_labels:
+            labels.append (label.unary_cost)
+        Theta.append(labels)   
+    return Theta     
+
+
 def dynamic_programming(nodes, edges):
-    F, ptr = None, None
-    return F, ptr
+    #checking wheather this a forward or a backward labeling
+    backward = False
+    if edges[0].left != 0:
+        backward = True
 
-def backtrack(nodes, edges, F, ptr):
-    assignment = [0] * len(nodes)
-    return assignment
+    # make sure that i only use the edges that would make the a chain (right = left - 1) 
+    #new_edges = [edge for edge in edges if (edge.right - edge.left == 1)]
+    
+    #Processed label is a named tuple (pseudo object) that i created to store the date of each label (bellman costs (F) , Unary cost (Theta) , connected label (to get the back tracing))
+    processed_label = namedtuple('processed_label', 'F unary_cost connected_label')
+    
+    #Processed node is a named tuple (pseudo object) that i created to store the list of processed labels in each node)
+    processed_node = namedtuple('processed_node', 'processed_labels')    
+    
+    #list of all processed nodes
+    processed_nodes = []    
+    for index , node in enumerate(nodes):
+        list_of_processed_labels = []
+        #for the first node all the labels in the node will have a bellman function of 0 - given in the lecture - and each label won't be conected to other nodes from the left (because it is the first node in the chain) 
+        if (index == 0):
+            for i,cost in enumerate(node.costs):
+              new_label = processed_label(F=0,unary_cost=node.costs[i],connected_label=None)
+              list_of_processed_labels.append(new_label)
+            curr_node = processed_node(processed_labels=list_of_processed_labels)
+            processed_nodes.append(curr_node)
 
+        else:
+            for curr_index,curr_label in enumerate (node.costs):
+                comparision_array = []
+                for prev_index,prev_label in enumerate (processed_nodes[index-1].processed_labels):
+                     # sum unary cost(modified node[0]) and the bellman function (modified node[1]) + the pairwise cost (the corresbonding edge)
+                     if backward :
+                         value = prev_label.F + prev_label.unary_cost + edges[index-1].costs[(curr_index,prev_index)]
+                     else:
+                        value = prev_label.F + prev_label.unary_cost + edges[index-1].costs[(prev_index,curr_index)]
+                     comparision_array.append(value)
+                     
+                new_label = processed_label(F=np.min(comparision_array),unary_cost=curr_label,connected_label=np.argmin(comparision_array))
+                list_of_processed_labels.append(new_label)
+            curr_node = processed_node(processed_labels=list_of_processed_labels) 
+            processed_nodes.append(curr_node)
+
+            if index == len(nodes)-1:
+                last_node_comparision_array =[]
+                for label in curr_node.processed_labels:
+                    last_node_comparision_array.append((label.unary_cost+label.F))
+                ptr = np.argmin(last_node_comparision_array)
+
+    return processed_nodes,ptr
+
+def backtrack(nodes, edges, processed_nodes, ptr):
+    processed_nodes = list(reversed(processed_nodes))
+    assignment=[ptr]
+    for index,node in enumerate(processed_nodes):
+        next_step = node.processed_labels[assignment[-1]].connected_label
+        if (next_step !=None):
+            assignment.append(next_step)
+ 
+    return list(reversed(assignment))
 
 # For exercise 1.5
+
 def compute_min_marginals(nodes, edges):
-    m = [[0 for l in n] for n in nodes]
-    return m
+
+    # run the algorithm forward 
+    processed_nodes,ptr = dynamic_programming(nodes, edges)
+    # run the algorithm Backward
+    processed_nodess_rev,ptr_rev = dynamic_programming(list(reversed(nodes)),list(reversed(edges))) 
+    F = get_Fn(processed_nodes)
+    B = get_Fn(list(reversed(processed_nodess_rev)))
+    Theta = get_Theta(processed_nodes)
+    Min_Marg = np.add(np.add(F,B), Theta)
+    
+    return Min_Marg
 
 
 # For execrise 1.6
@@ -60,3 +142,4 @@ def dynamic_programming_tree(nodes, edges):
 def backtrack_tree(nodes, edges, F, ptr):
     assignment = [0] * len(nodes)
     return assignment
+
