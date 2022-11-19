45dc5176054f03c6c9f7b888708724c11a95dc2f
shouryaverma <46604683+shouryaverma@users.noreply.github.com>
2022-11-19 11:51:43 +0100

    Add files via upload

--- ex1/image_example.py
+++ ex1/image_example.py
@@ -4,29 +4,97 @@
 
 # Before using this you have to install the Python Pillow and numpy libraries.
 
-from PIL import Image
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import imageio
+from numba import jit
 
-# To load an image:
-img = Image.open('seam_carving.jpg')
+#Sobel Img Edge Detection
+def edgeImg(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sobelX = cv2.Sobel(gray,cv2.CV_64F,1,0)
+    sobelY = cv2.Sobel(gray,cv2.CV_64F,0,1)
 
-# You can show the image in a new window:
-img.show()
+    return np.sqrt(np.power(sobelX,2) + np.power(sobelY,2))
 
-# You can create an numpy array out of an image. The underlying numpy data type
-# (dtype) will be chosen atomatically. For a normal RGB image you will get a
-# matrix of the shape `height x width x 3` of dtype np.uint8.
-data = np.asarray(img)
-row = 3
-column = 2
-pixel = data[row, column]
-r, g, b = pixel
+#use dynamic programming to find Min Energy Path
+@jit
+def calculateMinEnergyPath(energyImg):
+    minEnergyPath = np.zeros(energyImg.shape)
+    n = energyImg.shape[0]
+    m = energyImg.shape[1]
 
-# Note that if you want to do arithmetics on the data, you might want to change
-# the datatype as and np.uint8 will overflow.
-# For example: `np.uint(3) - np.uint(4) == 255`.
-print(np.abs(data[row-1, column].astype(np.float64) - data[row+1, column].astype(np.float64)).sum())
+    #base case on the last row of the matrix
+    for j in range(0,m):
+        minEnergyPath[n-1][j] = energyImg[n-1][j]
 
-# You create an image out of numpy array in the following way:
-img = Image.fromarray(data)
-img.save('out.png')
+    #builds matrix from bottom up
+    for i in range(n-2,-1,-1):
+        for j in range(0,m):
+            #just making sure we dont go out of bounds and finding the min
+            left = max(0,j-1)
+            right = min(m-1,j+1)
+            
+
+            minEnergyPath[i][j] = energyImg[i][j] + min(minEnergyPath[i+1][left:right+1])
+
+    return minEnergyPath
+
+@jit
+def removeSeam(img,minEnergyPath):
+    n = minEnergyPath.shape[0]
+    m = minEnergyPath.shape[1]
+    
+    minPos = 0
+    minVal = minEnergyPath[0][0]
+
+    #find Min Seam
+    for i in range(1,m):
+        if(minEnergyPath[0][i] < minVal):
+            minPos = i
+            minVal = minEnergyPath[0][i]
+
+    newImg = np.zeros((img.shape[0],img.shape[1]-1,img.shape[2]),dtype=np.uint8)
+
+    newImg[0] = np.concatenate((img[0][0:minPos],img[0][minPos+1:img.shape[1]]))
+    for i in range(1,n):
+        #find min from three down
+        center = minPos
+        min = minEnergyPath[i][center]
+
+        if(center>0 and minEnergyPath[i][center-1] < min):
+            min = minEnergyPath[i][center-1]
+            minPos = center -1
+
+        if(center<m-1 and minEnergyPath[i][center+1] < min):
+            min = minEnergyPath[i][center+1]
+            minPos = center+1
+
+        newImg[i] = np.concatenate((img[i][0:minPos],img[i][minPos+1:img.shape[1]]))
+    
+    return newImg
+
+
+def removeNSeams(img,seamNumber):
+    newImg = img
+    for i in range(0,seamNumber):
+        print ('removing seam ',i+1, ' of ', seamNumber, end="\r")
+        
+        newEP = calculateMinEnergyPath(edgeImg(newImg))
+        newImg = removeSeam(newImg,newEP)
+
+
+    return newImg
+
+img = imageio.imread('tower.jpg')
+
+newImg = removeNSeams(img,40)
+
+plt.figure(figsize=(10,10))
+plt.imshow(img)
+plt.show()
+
+plt.figure(figsize=(10,10))
+plt.imshow(newImg)
+plt.show()
